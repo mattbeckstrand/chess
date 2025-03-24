194f20a07341cf5265d8d0b4c5c3719db03280a2
Matt Beckstrand <matt.beckstrand@gmail.com>
2025-03-24 12:11:59 -0600

    "Added functions for ServerFacade "

---
+++ shared/src/main/java/server/ServerFacade.java
@@ -0,0 +1,114 @@
+package server;
+
+import com.google.gson.Gson;
+import exception.ResponseException;
+import model.AuthData;
+import model.LoginRequest;
+import model.UserData;
+
+
+import java.io.*;
+import java.net.*;
+
+public class ServerFacade {
+    private final String serverUrl;
+
+    public ServerFacade(String url) {
+        serverUrl = url;
+    }
+
+    public AuthData Register(UserData userData) throws ResponseException{
+        var path = "/user";
+        return this.makeRequest("POST", path, userData, AuthData.class);
+    }
+
+    public String Login(LoginRequest loginRequest) throws ResponseException{
+        var path = "/session";
+        var response = this.makeRequest("POST", path, loginRequest, AuthData.class);
+        return response.authToken();
+    }
+
+    public void Logout(String authToken) throws ResponseException{
+        var path = "/session";
+        this.makeRequestWithAuth("DELETE", path, authToken);
+    }
+
+    public Integer CreateGame(String gameName) throws ResponseException{
+        var path = "/game";
+        response
+    }
+
+
+    private <T> T makeRequest(String method, String path, Object request, Class<T> responseClass) throws ResponseException {
+        try {
+            URL url = (new URI(serverUrl + path)).toURL();
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            http.setRequestMethod(method);
+            http.setDoOutput(true);
+
+            writeBody(request, http);
+            http.connect();
+            throwIfNotSuccessful(http);
+            return readBody(http, responseClass);
+        } catch (ResponseException ex) {
+            throw ex;
+        } catch (Exception ex) {
+            throw new ResponseException(500, ex.getMessage());
+        }
+    }
+
+    private void makeRequestWithAuth(String method, String path, String authToken) throws ResponseException{
+        try{
+            URL url = (new URI(serverUrl + path)).toURL();
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            http.setRequestMethod(method);
+            http.setRequestProperty("Authorization", authToken);
+            http.connect();
+            throwIfNotSuccessful(http);
+        } catch (ResponseException ex) {
+            throw ex;
+        } catch (Exception ex) {
+            throw new ResponseException(500, ex.getMessage());
+        }
+    }
+
+    private static void writeBody(Object request, HttpURLConnection http) throws IOException {
+        if (request != null) {
+            http.addRequestProperty("Content-Type", "application/json");
+            String reqData = new Gson().toJson(request);
+            try (OutputStream reqBody = http.getOutputStream()) {
+                reqBody.write(reqData.getBytes());
+            }
+        }
+    }
+
+    private void throwIfNotSuccessful(HttpURLConnection http) throws IOException, ResponseException {
+        var status = http.getResponseCode();
+        if (!isSuccessful(status)) {
+            try (InputStream respErr = http.getErrorStream()) {
+                if (respErr != null) {
+                    throw ResponseException.fromJson(respErr);
+                }
+            }
+
+            throw new ResponseException(status, "other failure: " + status);
+        }
+    }
+    private static <T> T readBody(HttpURLConnection http, Class<T> responseClass) throws IOException {
+        T response = null;
+        if (http.getContentLength() < 0) {
+            try (InputStream respBody = http.getInputStream()) {
+                InputStreamReader reader = new InputStreamReader(respBody);
+                if (responseClass != null) {
+                    response = new Gson().fromJson(reader, responseClass);
+                }
+            }
+        }
+        return response;
+    }
+
+
+    private boolean isSuccessful(int status) {
+        return status / 100 == 2;
+    }
+}
